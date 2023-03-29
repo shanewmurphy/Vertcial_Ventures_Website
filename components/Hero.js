841867d44222ec80bfce3395f273eb3f2df6a636
shanewmurphy <shn.murphy@gmail.com>
2023-03-29 11:28:47 +0100

    Hero section

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,6 @@ export default function Hero() {
       <div className="my-container">
         <div className="flex justify-between nav-mobile">
           <div>
-            {" "}
             <Image
               src="/imgs/Logo_Blue.svg"
               width={124}
@@ -45,24 +44,24 @@ export default function Hero() {
           <div>BBBB</div>
         </div>
         <div className="container pt-8">
-          <h1 className="text-white text-display font-black uppercase antialiased">
+          <h1 className="text-white font-black uppercase antialiased">
             Cork Indoor <br />
             Rock Climbing
           </h1>
-          <h2 className="text-slateblue text-4xl mt-2 font-bold antialiased">
+          <h3 className="text-slateblue mt-1 font-bold antialiased">
             Climbing, Fitness and Fun
-          </h2>
-          <h3 className="text-2xl text-white lg:w-1/2  font-medium mt-4 antialiased">
+          </h3>
+          <h4 className="text-white lg:w-1/2  font-medium mt-2 antialiased">
             Experience the thrill of climbing in a safe and controlled
             environment
-          </h3>
+          </h4>
           <div>
             <div className="flex mt-6">
               <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-4">
                 <div>
                   <button className="button button1">Book Now</button>
                 </div>
-                <div className="pt-4 text-white text-3xl text-bold">
+                <div className="pt-4 text-white text-3xl text-bold xs:invisible lg:visible">
                   Call 0214 56874
                 </div>
               </div>
