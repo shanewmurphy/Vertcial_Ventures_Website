4513dc0762a15816ca8dfd28d26f2d907970d456
shanewmurphy <shn.murphy@gmail.com>
2023-03-31 14:55:35 +0100

    facilities section

--- components/Facility.js
+++ components/Facility.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Climbs() {
   return (
     <div>
@@ -7,10 +9,55 @@ export default function Climbs() {
             Our facilities have walls of different heights ranging from 3 to 32
             feet
           </h2>
-          <h4 className="text-center text-gray font-medium">
-            With over 10,000 square feet of climbing opportunities, there&apos;s
-            lots to choose from
-          </h4>
+          <div>
+            <h5 className="text-center text-gray font-bold">
+              With over 10,000 square feet of climbing opportunities,
+              there&apos;s lots to choose from
+            </h5>
+          </div>
+        </div>
+        <div className="my-container">
+          <div className="grid xl:grid-cols-2 lg:grid-cols-2 sm:grid-cols-1 gap-6 mb-32">
+            <div className="mt-8">
+              <Image
+                className="image-container"
+                src="/imgs/Boulding-min.png"
+                width={638}
+                height={428}
+                alt="Boulding walls Ventical Ventures"
+              />
+              <h3 className="text-center font-bold mt-2 mb-1 text-midnight">
+                Bouldering
+              </h3>
+              <p className="font-medium text-darkblue">
+                <small>
+                  150m2 of climbing surface offering a variety of terrain in one
+                  area, it&apos;s made up of slab climbing and slightly off
+                  vertical terrain on the sides, and steep climbing with unique,
+                  dramatic angles a prow and a cave feature.
+                </small>
+              </p>
+            </div>
+            <div className="mt-8">
+              <Image
+                className="image-container"
+                src="/imgs/Roped_Climbing-min.png"
+                width={638}
+                height={428}
+              />
+              <h3 className="text-center font-bold mt-2 mb-1 text-midnight">
+                Roped Climbing
+              </h3>
+              <p className="font-medium text-darkblue">
+                <small>
+                  Our rope climbing area has walls 10m high, with route lengths
+                  up to 14m in area&apos;s. Offering 500m2 of climbing, it
+                  features 32 lines, 28 of which can be led, 12 lines
+                  pre-equipped with Top Ropes.
+                </small>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
