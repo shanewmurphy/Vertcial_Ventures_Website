d39ba4f49bcd96563ec92699e528539e2337e745
shanewmurphy <shn.murphy@gmail.com>
2023-04-08 10:35:30 +0100

    Intro section fonts sizes

--- components/Intro.js
+++ components/Intro.js
@@ -3,16 +3,16 @@ export default function Intro() {
   return (
     <div className="full-width-container bg-midnight pb-32">
       <div className="my-container">
-        <div className="grid lg:grid-cols-2 sm:grid-col-1 gap-14">
+        <div className="grid lg:grid-cols-2 sm:grid-col-1 gap-20">
           <div>
-            <h2 className="text-slateblue font-black xl:text-6xl lg:text-4xl md:text-4xl sm:text-3xl mb-6 antialiased">
+            <h2 className="text-slateblue font-black xl:text-5xl lg:text-4xl md:text-5xl sm:text-3xl mb-4 antialiased">
               Vertical Ventures is the only indoor rock climbing facility in
               Cork
             </h2>
-            <h4 className="text-white mb-2 font-bold antialiased">
+            <h4 className="text-white mb-2 font-bold lg:text-2xl md:text-3xl sm:text-2xl antialiased">
               Improve your strength, balance, and flexibility with every climb
             </h4>
-            <p className="text-white font-normal antialiased">
+            <p className="text-white text-lg font-medium antialiased">
               Our experienced staff is here to support and encourage you every
               step of the way. Learn from experienced instructors and take your
               climbing to the next level.
